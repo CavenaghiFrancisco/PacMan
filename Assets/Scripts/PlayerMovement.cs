f76e6326e30cf69e022594922e09440f0754705c
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2022-05-23 14:56:56 -0300

    Added phton view.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        view = GetComponent<PhotonView>();
     }
 
     private void Update()
